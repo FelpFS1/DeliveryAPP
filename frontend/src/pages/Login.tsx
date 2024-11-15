1edda990707325fe2b94778077cf6954b51c359c
FelpFS1 <phff800@gmail.com>
2024-11-15 18:04:59 -0300

    feat: loading component implements in login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -9,37 +9,46 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import type { RootState } from "@/redux/store";
+import Loading from "@/components/Loading";
 
 export default function LoginPage() {
-  const { userId } = useAuth();
+  const { userId, isLoaded } = useAuth();
   const navigate = useNavigate();
   const { theme } = useSelector((state: RootState) => state.theme);
   useEffect(() => {
+    if (!isLoaded) return;
+
     if (userId) {
       navigate("/");
     }
-  }, [navigate, userId]);
+  }, [isLoaded, navigate, userId]);
   return (
-    <div className="grid grid-cols-2">
-      <div className="flex h-screen flex-col items-center justify-center gap-3 p-5">
-        <ThemeButton position="left-1 top-2" />
-        <h1 className="text-4xl font-bold">Bem-vindo (a)!</h1>
-        <p className="text-zinc-400">
-          Faça login ou cadastre-se para uma saborosa experiência!
-        </p>
-        <SignInButton mode="modal">
-          <Button
-            variant={theme === "dark" ? "outline" : "default"}
-            className="font-bold"
-          >
-            <LogInIcon />
-            Fazer login ou criar conta
-          </Button>
-        </SignInButton>
-      </div>
-      <div className="h-[100vh] w-full">
-        <img src={loginPageBg} alt="" className="h-[100vh] w-full" />
-      </div>
-    </div>
+    <>
+      {isLoaded ? (
+        <div className="grid grid-cols-2">
+          <div className="flex h-screen flex-col items-center justify-center gap-3 p-5">
+            <ThemeButton position="left-1 top-2" />
+            <h1 className="text-4xl font-bold">Bem-vindo (a)!</h1>
+            <p className="text-zinc-400">
+              Faça login ou cadastre-se para uma saborosa experiência!
+            </p>
+            <SignInButton mode="modal">
+              <Button
+                variant={theme === "dark" ? "outline" : "default"}
+                className="font-bold"
+              >
+                <LogInIcon />
+                Fazer login ou criar conta
+              </Button>
+            </SignInButton>
+          </div>
+          <div className="h-[100vh] w-full">
+            <img src={loginPageBg} alt="" className="h-[100vh] w-full" />
+          </div>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 }
